a70911371ccc66115294cd527f16ee591dfe145e
Nick McIntyre <nick@mcintyre.io>
2021-01-10 11:27:34 -0600

    YAGNI Brython stdlib

--- client/modules/IDE/reducers/files.js
+++ client/modules/IDE/reducers/files.js
@@ -13,9 +13,8 @@ const defaultHTML =
 `<!DOCTYPE html>
 <html lang="en">
   <head>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/p5@1.2.0/lib/p5.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/brython@3.9.0/brython.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/brython@3.9.0/brython_stdlib.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="pyp5.js"></script>    
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -1254,7 +1253,7 @@ const jqueryWrapper =
       window.instance.canvas.remove();
     }
     $('#sketch').html(code);
-    brython(1);
+    brython(0);
   }, 'text');
 });
 `;
